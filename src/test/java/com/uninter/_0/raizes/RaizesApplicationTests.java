6c316aba7e41b38f70ec7d20ec20ff5902d676d1
Mateus Bonfanti <matebonfanti@gmail.com>
2026-05-23 18:11:39 -0300

    First Commit

---
+++ src/test/java/com/uninter/_0/raizes/RaizesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninter._0.raizes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
